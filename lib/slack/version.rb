61386aabe4c2caac164a958e2ba2adaf6df0944d
starwarrs <yulikamiya@gmail.com>
2019-12-03 19:24:52 +0900

    wip

--- lib/slack/version.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-module Slack
-  VERSION = '0.14.5'
-end
--- lib/slack/web/api/mixins.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-require_relative 'mixins/channels.id'
-require_relative 'mixins/users.id'
-require_relative 'mixins/users.search'
-require_relative 'mixins/groups.id'
