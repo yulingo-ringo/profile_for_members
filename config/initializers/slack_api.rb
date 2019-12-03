f467b86e9e8e01c448a7d3f743c3088a42792c94
starwarrs <yulikamiya@gmail.com>
2019-12-03 19:21:58 +0900

    wip

--- config/initializers/slack_api.rb
+++ config/initializers/slack_api.rb
@@ -1,13 +1,13 @@
-require 'slack'
+# require 'slack'
 
-Slack.configure do |config|
-  config.token = ENV['BOT_TOKEN']
-end
+# Slack.configure do |config|
+#   config.token = ENV['BOT_TOKEN']
+# end
 
-Slack::Web::Client.config do |config|
-  config.user_agent = 'Slack Ruby Client/1.0'
-end
+# Slack::Web::Client.config do |config|
+#   config.user_agent = 'Slack Ruby Client/1.0'
+# end
 
-Slack::RealTime::Client.config do |config|
-  config.start_method = :rtm_start
-end
+# Slack::RealTime::Client.config do |config|
+#   config.start_method = :rtm_start
+# end
