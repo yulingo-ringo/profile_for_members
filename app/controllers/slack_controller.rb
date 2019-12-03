9730ffec418cf97629e17b6ffd524dcd5b6e6ccd
starwarrs <yulikamiya@gmail.com>
2019-12-03 19:12:45 +0900

    wip

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -7,7 +7,7 @@ class SlackController < ApplicationController
         @body = JSON.parse(request.body.read)
         case @body['type']
         when 'url_verification'
-          render json: @body
+          render json: @body['challenge']
         when 'event_callback'
           # ..
         end
