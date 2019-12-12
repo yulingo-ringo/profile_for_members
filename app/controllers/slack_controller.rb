f0bdd0ee30ee2bc75760cd90d63c514afba3ce79
starwarrs <yulikamiya@gmail.com>
2019-12-12 19:14:19 +0900

    wip

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -4,6 +4,7 @@ class SlackController < ApplicationController
     end
 
     def create
+        p params
         @body = JSON.parse(request.body.read)
         case @body['type']
         when 'url_verification'
