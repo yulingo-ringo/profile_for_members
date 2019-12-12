9be2b5058619966db2a8c2d146f7fc3a395b52b6
starwarrs <yulikamiya@gmail.com>
2019-12-12 19:39:33 +0900

    wip

--- app/services/body/test_service.rb
+++ app/services/body/test_service.rb
@@ -37,50 +37,8 @@ module Body
           :channel => :self.json["event"]["channel"],
           :text  => "sorry, I don't understand. Please mention someone ¯\_(ツ)_/¯"
         }
+      end
     end
-
-
-    
-    # def web_client
-    #   @web_client ||= Slack::Web::Client.new
-    # end
-      
-    # def real_time_client
-    #   @real_time_client ||= Slack::RealTime::Client.new
-    # end
-
-    
-    
-    # client=Slack::RealTime::Client.new
-  
-    # client.on :hello do
-    #     puts(
-    #       "Successfully connected, welcome '#{client.self.name}' to " \
-    #       "the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
-    #     )
-    # end
-      
-    # client.on :message do |data|
-    #   puts data
-    
-    #   client.typing channel: data.channel
-    
-    #   case data.text
-    #   when "@#{user_id}"
-    #     client.message channel: data.channel, text: "@#{user_id}is not ready"
-    #   when /^bot/
-    #     client.message channel: data.channel, text: "Sorry,I'm not prepared for that response"
-    #   end
-    # end
-      
-    # client.on :close do |_data|
-    #   puts 'Connection closing, exiting.'
-    # end
-      
-    # client.on :closed do |_data|
-    #   puts 'Connection has been disconnected.'
-    # end
-      
   end
 end
 
