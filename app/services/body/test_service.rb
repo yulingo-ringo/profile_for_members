74d970afc0e456d6b1f0c493c2d7666735deb1bf
starwarrs <yulikamiya@gmail.com>
2019-12-14 16:06:14 +0900

    wip

--- app/services/body/test_service.rb
+++ app/services/body/test_service.rb
@@ -18,6 +18,7 @@ module Body
       #送られて来たメッセージが自分へのメンションなのか他人へのメンションなのか全く関係のないものなのかで場合分け
       if @json[:event][:text] =="<@#{@json[:event][:user]}>"#自分の時
         json_str='{
+          "token": ENV['BOT_OAUTH_TOKEN'],
           "ok": true,
           "channel": "CP9RQQL7P",
           "ts": "1576302514.000100",
