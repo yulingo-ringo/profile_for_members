aa4564834d62d9408b14fa83e6ebd1d6e50db0c8
starwarrs <yulikamiya@gmail.com>
2019-12-23 13:36:25 +0900

    wip

--- app/services/body/test_service.rb
+++ app/services/body/test_service.rb
@@ -39,7 +39,7 @@ module Body
                 req.url '/api/users.list'
                 req.params[:token] = ENV['SLACK_BOT_USER_TOKEN']
               end
-              info = JSON.parse(response)
+              info = JSON.parse(response&.body || "{}")
               p info
         else
             body = {
