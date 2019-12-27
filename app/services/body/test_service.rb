01922b8ca027ebb6b2d90e07d781ba3c790b1824
starwarrs <yulikamiya@gmail.com>
2019-12-27 17:22:42 +0900

    wip

--- app/services/body/test_service.rb
+++ app/services/body/test_service.rb
@@ -52,7 +52,7 @@ module Body
                 body = {
                   :token => ENV['SLACK_BOT_USER_TOKEN'],
                   :channel => @json[:event][:channel],
-                  :text  => "#{member["profile"]["display_name"]}"
+                  :text  => "#{member["profile"]["real_name"]}"
                 }
                 conn.post '/api/chat.postMessage',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}
               end
