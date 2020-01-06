7cae0454b97d238a0c19046b87ddfd97f13e7964
starwarrs <yulikamiya@gmail.com>
2020-01-06 15:30:09 +0900

    wip

--- app/services/body/action.rb
+++ app/services/body/action.rb
@@ -10,68 +10,69 @@ module Body
               builder.use Faraday::Adapter::NetHttp     # Net/HTTP をアダプターに使う
           end
           p @json["message"]["blocks"][0]["elements"][0]["text"]["text"]
-          view = {
-              "type": "modal",
-              "title": {
-                  "type": "plain_text",
-                  "text": "Modal title"
-              },
-              "blocks": [
-                  {
-                  "type": "section",
-                  "text": {
-                      "type": "mrkdwn",
-                      "text": "あなたが好きな映画は？"
-                  },
-                  "block_id": "section1",
-                  "accessory": {
-                      "type": "button",
-                      "text": {
-                      "type": "plain_text",
-                      "text": "Click me"
-                      },
-                      "action_id": "button_abc",
-                      "value": "Button value",
-                      "style": "danger"
-                  }
-                  },
-                  {
-                  "type": "input",
-                  "label": {
-                      "type": "plain_text",
-                      "text": "Input label"
-                  },
-                  "element": {
-                      "type": "plain_text_input",
-                      "action_id": "input1",
-                      "placeholder": {
-                      "type": "plain_text",
-                      "text": "Type in here"
-                      },
-                      "multiline": false
-                  },
-                  "optional": false
-                  }
-              ],
-              "close": {
-                  "type": "plain_text",
-                  "text": "Cancel"
-              },
-              "submit": {
-                  "type": "plain_text",
-                  "text": "Save"
-              },
-              "private_metadata": "Shhhhhhhh",
-              "callback_id": "view_identifier_12"
-              }
+          if @json["message"]["blocks"][0]["elements"][0]["text"]["text"] == "Fill+in+the+Blank"
+            view = {
+                "type": "modal",
+                "title": {
+                    "type": "plain_text",
+                    "text": "Modal title"
+                },
+                "blocks": [
+                    {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "あなたが好きな映画は？"
+                    },
+                    "block_id": "section1",
+                    "accessory": {
+                        "type": "button",
+                        "text": {
+                        "type": "plain_text",
+                        "text": "Click me"
+                        },
+                        "action_id": "button_abc",
+                        "value": "Button value",
+                        "style": "danger"
+                    }
+                    },
+                    {
+                    "type": "input",
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Input label"
+                    },
+                    "element": {
+                        "type": "plain_text_input",
+                        "action_id": "input1",
+                        "placeholder": {
+                        "type": "plain_text",
+                        "text": "Type in here"
+                        },
+                        "multiline": false
+                    },
+                    "optional": false
+                    }
+                ],
+                "close": {
+                    "type": "plain_text",
+                    "text": "Cancel"
+                },
+                "submit": {
+                    "type": "plain_text",
+                    "text": "Save"
+                },
+                "private_metadata": "Shhhhhhhh",
+                "callback_id": "view_identifier_12"
+                }
 
-              body = {
-              :token => ENV['SLACK_BOT_USER_TOKEN'],#あとでherokuで設定します
-              :trigger_id => @json["trigger_id"],#こうするとDM内に返信できます
-              :view => view
-              }
-          conn.post '/api/views.open',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}#ヘッダーはつけなければいけないらしい、このままで大丈夫です。
-            
+                body = {
+                :token => ENV['SLACK_BOT_USER_TOKEN'],#あとでherokuで設定します
+                :trigger_id => @json["trigger_id"],#こうするとDM内に返信できます
+                :view => view
+                }
+            conn.post '/api/views.open',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}#ヘッダーはつけなければいけないらしい、このままで大丈夫です。
+          end    
       end
   end   
 end
