ee7e75b2c7a60d29ac24cdd5a9f34ff17164afb0
starwarrs <yulikamiya@gmail.com>
2020-01-12 15:06:25 +0900

    wip

--- app/services/body/test_service.rb
+++ app/services/body/test_service.rb
@@ -4,22 +4,18 @@ module Body
       @json=json
     end
     def execute
-      #p @json
-      #Faradayを使って、JSON形式のファイルをPOSTできるようにする
       conn = Faraday::Connection.new(:url => 'https://slack.com') do |builder|
-        builder.use Faraday::Request::UrlEncoded  # リクエストパラメータを URL エンコードする
-        builder.use Faraday::Response::Logger     # リクエストを標準出力に出力する
-        builder.use Faraday::Adapter::NetHttp     # Net/HTTP をアダプターに使う
+        builder.use Faraday::Request::UrlEncoded  
+        builder.use Faraday::Response::Logger     
+        builder.use Faraday::Adapter::NetHttp    
       end
 
       p @json[:event][:text]
       p "<@#{@json[:event][:user]}>"
       p ENV["SLACK_BOT_USER_TOKEN"]
-
-      #送られて来たメッセージが自分へのメンションなのか他人へのメンションなのか全く関係のないものなのかで場合分け
       if @json[:event][:subtype] != "bot_message"
         if @json[:event][:text].include?("<@")
-          if @json[:event][:text] =="<@#{@json[:event][:user]}>"#自分の時
+          if @json[:event][:text] =="<@#{@json[:event][:user]}>"
             text="<@#{@json[:event][:user]}>まだURLが用意されていません。"  
           else 
             text ="その人はまだURLが用意できていません"
@@ -31,36 +27,36 @@ module Body
             }
             conn.post '/api/chat.postMessage',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}
         elsif @json[:event][:text].include?("info") || @json[:event][:text].include?("help")
-              response = conn.get do |req|  
-                req.url '/api/users.list'
-                req.params[:token] = ENV['SLACK_BOT_USER_TOKEN']
-              end
-              info = JSON.parse(response&.body)
-              members=info["members"]
+              #response = conn.get do |req|  
+              #  req.url '/api/users.list'
+              #  req.params[:token] = ENV['SLACK_BOT_USER_TOKEN']
+              #end
+              # info = JSON.parse(response&.body)
+              # members=info["members"]
               body = {
                 :token => ENV['SLACK_BOT_USER_TOKEN'],
                 :channel => @json[:event][:channel],
-                :text  => "お困りですか？"
+                :text  => "@名前でメンションしてプロフィールがチェックできます"
               }
               conn.post '/api/chat.postMessage',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}
 
-              members.each do |member|
-                if member["is_bot"]==false&& member["profile"]["real_name"]!="Slackbot"
-                  body = {
-                    :token => ENV['SLACK_BOT_USER_TOKEN'],
-                    :channel => @json[:event][:channel],
-                    :text  => "#{member["profile"]["real_name"]}"
-                  }
-                  conn.post '/api/chat.postMessage',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}
-                end
-              end
+              # members.each do |member|
+              #   if member["is_bot"]==false&& member["profile"]["real_name"]!="Slackbot"
+              #     body = {
+              #       :token => ENV['SLACK_BOT_USER_TOKEN'],
+              #       :channel => @json[:event][:channel],
+              #       :text  => "#{member["profile"]["real_name"]}"
+              #     }
+              #     conn.post '/api/chat.postMessage',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}
+              #   end
+              # end
 
-              body = {
-                :token => ENV['SLACK_BOT_USER_TOKEN'],
-                :channel => @json[:event][:channel],
-                :text  => "この中のあなたが興味ある人をメンションしてください。名前の前に@をつけるとメンションをすることができます。"
-              }
-              conn.post '/api/chat.postMessage',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}
+              # body = {
+              #   :token => ENV['SLACK_BOT_USER_TOKEN'],
+              #   :channel => @json[:event][:channel],
+              #   :text  => "この中のあなたが興味ある人をメンションしてください。名前の前に@をつけるとメンションをすることができます。"
+              # }
+              # conn.post '/api/chat.postMessage',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}
         elsif @json[:event][:text].include?("database")
               body = {
                 :token => ENV['SLACK_BOT_USER_TOKEN'],
