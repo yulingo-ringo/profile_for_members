c7a76a246b68015e0e9b221e236d777803514ecf
starwarrs <yulikamiya@gmail.com>
2020-01-25 20:09:02 +0900

    wip

--- app/services/body/lambda.rb
+++ app/services/body/lambda.rb
@@ -9,14 +9,7 @@ module Body
         builder.use Faraday::Adapter::NetHttp    
       end
       
-      body = {
-        :token => ENV['SLACK_BOT_USER_TOKEN'],
-        :channel => "#general",
-        :text  => "あなたに質問があります"
-        
-      }
-      conn.post '/api/chat.postMessage',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}
-
+   
       response = conn.get do |req|  
         req.url '/api/conversations.list'
         req.params[:token] = ENV['SLACK_BOT_USER_TOKEN']
@@ -26,6 +19,13 @@ module Body
       p hash["channels"]
       for var in hash["channels"] do
         p var["id"]
+        body = {
+          :token => ENV['SLACK_BOT_USER_TOKEN'],
+          :channel => "#{var["id"]}",
+          :text  => "あなたに質問があります"
+          
+        }
+        conn.post '/api/chat.postMessage',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}
       end
       p "ハッシュかされてる？"
 
