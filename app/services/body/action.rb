0c101cf684f4c12ae5ae5d737b57d53d7804d733
starwarrs <yulikamiya@gmail.com>
2020-01-26 17:50:44 +0900

    wip

--- app/services/body/action.rb
+++ app/services/body/action.rb
@@ -36,7 +36,72 @@ module Body
               break
             end
            end
-            p @json["user"]
+
+           if @json["type"]=="block_actions"
+            view = {
+              "type": "modal",
+              "title": {
+                  "type": "plain_text",
+                  "text": "質問箱"
+              },
+              "blocks": [
+                  {
+                  "type": "section",
+                  "text": {
+                      "type": "mrkdwn",
+                      "text": "あなたが好きな映画は？"
+                  },
+                  "block_id": "section1",
+                   "accessory": {
+                       "type": "button",
+                       "text": {
+                       "type": "plain_text",
+                       "text": "Click me"
+                       },
+                       "action_id": "button_abc",
+                       "value": "Button value",
+                       "style": "danger"
+                   }
+                   },
+                  {
+                  "type": "input",
+                  "label": {
+                      "type": "plain_text",
+                      "text": "Input label"
+                  },
+                  "element": {
+                      "type": "plain_text_input",
+                      "action_id": "input1",
+                      "placeholder": {
+                      "type": "plain_text",
+                      "text": "Type in here"
+                      },
+                      "multiline": false
+                  },
+                  "optional": false
+                  }
+              ],
+              "close": {
+                  "type": "plain_text",
+                  "text": "Cancel"
+              },
+              "submit": {
+                  "type": "plain_text",
+                  "text": "Save"
+              },
+              "private_metadata": "Shhhhhhhh",
+              "callback_id": "view_identifier_12"
+              }
+
+              body = {
+              :token => ENV['SLACK_BOT_USER_TOKEN'],#あとでherokuで設定します
+              :trigger_id => @json["trigger_id"],#こうするとDM内に返信できます
+              :view => view
+              }
+            conn.post '/api/views.open',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}#ヘッダーはつけなければいけないらしい、このままで大丈夫です。
+           
+           else
+            @json["user"]
             response = natsuo.get do |req|  
                 req.url '/login'
              #   req.headers['Content-Type'] = 'application/html'
@@ -54,68 +119,7 @@ module Body
                 p req.body
               end
             
-             view = {
-                 "type": "modal",
-                 "title": {
-                     "type": "plain_text",
-                     "text": "質問箱"
-                 },
-                 "blocks": [
-                     {
-                     "type": "section",
-                     "text": {
-                         "type": "mrkdwn",
-                         "text": "あなたが好きな映画は？"
-                     },
-                     "block_id": "section1",
-                      "accessory": {
-                          "type": "button",
-                          "text": {
-                          "type": "plain_text",
-                          "text": "Click me"
-                          },
-                          "action_id": "button_abc",
-                          "value": "Button value",
-                          "style": "danger"
-                      }
-                      },
-                     {
-                     "type": "input",
-                     "label": {
-                         "type": "plain_text",
-                         "text": "Input label"
-                     },
-                     "element": {
-                         "type": "plain_text_input",
-                         "action_id": "input1",
-                         "placeholder": {
-                         "type": "plain_text",
-                         "text": "Type in here"
-                         },
-                         "multiline": false
-                     },
-                     "optional": false
-                     }
-                 ],
-                 "close": {
-                     "type": "plain_text",
-                     "text": "Cancel"
-                 },
-                 "submit": {
-                     "type": "plain_text",
-                     "text": "Save"
-                 },
-                 "private_metadata": "Shhhhhhhh",
-                 "callback_id": "view_identifier_12"
-                 }
-
-                 body = {
-                 :token => ENV['SLACK_BOT_USER_TOKEN'],#あとでherokuで設定します
-                 :trigger_id => @json["trigger_id"],#こうするとDM内に返信できます
-                 :view => view
-                 }
-             conn.post '/api/views.open',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}#ヘッダーはつけなければいけないらしい、このままで大丈夫です。
-              
+            end
       end
   end   
 end
