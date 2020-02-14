e21f20e2fdf0564e56125e9eaf08b84dd6c8bb11
starwarrs <yulikamiya@gmail.com>
2020-02-15 02:35:21 +0900

    wip

--- app/services/body/lambda.rb
+++ app/services/body/lambda.rb
@@ -36,7 +36,7 @@ module Body
          :content => hashed_question[:content]
        }
 
-      natsuo.post '/api/chat.postMessage',body.to_json, {"workspace_id" => '???',"slack_user_id"=>"???"}
+      natsuo.post '/api/chat.postMessage',body.to_json, {"workspace_id" => 'TPUL203HT',"slack_user_id"=>"UPH64QN9Z"}
 
       hash = JSON.parse(response.body)
       body = {
