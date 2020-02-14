867d306e61a1c995ec93a26d32bce137b1759bc0
starwarrs <yulikamiya@gmail.com>
2020-02-15 01:42:00 +0900

    wip

--- app/services/body/action.rb
+++ app/services/body/action.rb
@@ -17,78 +17,80 @@ module Body
           p "これがアクションのjson"
           p @json
           p "次はjsonのアクション"
-          p @json["actions"][0]
+          p @json["actions"][0]["value"]
           p "ここまで"
           
            if @json["type"]=="block_actions"
-            response = conn.get do |req|  
-                req.url '/api/users.list'
-                req.params[:token] = ENV['SLACK_BOT_USER_TOKEN']
-              end
-               info = JSON.parse(response&.body)
-               members=info["members"]
-               for var in members do
-                p "テキストとその下は名前！"
-                p @json["message"]["text"]
-                p var["profile"]["real_name"]
-                if @json["message"]["text"].include?(var["profile"]["real_name"])
-                  p "下がアクションの名前"
-                  p var["id"]
-                  break
-                end
-               end
-            view = {
-              "type": "modal",
-              "title": {
-                  "type": "plain_text",
-                  "text": "質問箱"
-              },
-              "blocks": [
-                  {
-                  "type": "section",
-                  "text": {
-                      "type": "mrkdwn",
-                      "text": "あなたが好きな映画は？"
-                  },
-                  "block_id": "section1",
-                   },
-                  {
-                  "type": "input",
-                  "label": {
-                      "type": "plain_text",
-                      "text": "Input label"
-                  },
-                  "element": {
-                      "type": "plain_text_input",
-                      "action_id": "input1",
-                      "placeholder": {
-                      "type": "plain_text",
-                      "text": "Type in here"
-                      },
-                      "multiline": false
-                  },
-                  "optional": false
-                  }
-              ],
-              "close": {
-                  "type": "plain_text",
-                  "text": "Cancel"
-              },
-              "submit": {
-                  "type": "plain_text",
-                  "text": "Save"
-              },
-              "private_metadata": "Shhhhhhhh",
-              "callback_id": "view_identifier_12"
-              }
-
-              body = {
-              :token => ENV['SLACK_BOT_USER_TOKEN'],#あとでherokuで設定します
-              :trigger_id => @json["trigger_id"],#こうするとDM内に返信できます
-              :view => view
-              }
-            conn.post '/api/views.open',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}#ヘッダーはつけなければいけないらしい、このままで大丈夫です。
-           
+                if @json["actions"][0]["value"]=="link"
+                    response = conn.get do |req|  
+                        req.url '/api/users.list'
+                        req.params[:token] = ENV['SLACK_BOT_USER_TOKEN']
+                    end
+                    info = JSON.parse(response&.body)
+                    members=info["members"]
+                    for var in members do
+                        p "テキストとその下は名前！"
+                        p @json["message"]["text"]
+                        p var["profile"]["real_name"]
+                        if @json["message"]["text"].include?(var["profile"]["real_name"])
+                        p "下がアクションの名前"
+                        p var["id"]
+                        break
+                        end
+                    end
+                    
+                else
+                    view = {
+                        "type": "modal",
+                        "title": {
+                            "type": "plain_text",
+                            "text": "質問箱"
+                        },
+                        "blocks": [
+                            {
+                            "type": "section",
+                            "text": {
+                                "type": "mrkdwn",
+                                "text": "あなたが好きな映画は？"
+                            },
+                            "block_id": "section1",
+                            },
+                            {
+                            "type": "input",
+                            "label": {
+                                "type": "plain_text",
+                                "text": "Input label"
+                            },
+                            "element": {
+                                "type": "plain_text_input",
+                                "action_id": "input1",
+                                "placeholder": {
+                                "type": "plain_text",
+                                "text": "Type in here"
+                                },
+                                "multiline": false
+                            },
+                            "optional": false
+                            }
+                        ],
+                        "close": {
+                            "type": "plain_text",
+                            "text": "Cancel"
+                        },
+                        "submit": {
+                            "type": "plain_text",
+                            "text": "Save"
+                        },
+                        "private_metadata": "Shhhhhhhh",
+                        "callback_id": "view_identifier_12"
+                        }
+    
+                        body = {
+                        :token => ENV['SLACK_BOT_USER_TOKEN'],#あとでherokuで設定します
+                        :trigger_id => @json["trigger_id"],#こうするとDM内に返信できます
+                        :view => view
+                        }
+                        conn.post '/api/views.open',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}#ヘッダーはつけなければいけないらしい、このままで大丈夫です。
            else
             response = conn.get do |req|  
               req.url '/api/users.list'
