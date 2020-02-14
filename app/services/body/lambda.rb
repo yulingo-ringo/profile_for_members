bb0251d9eeaf3e5a6b3f546752eeb9f3c77ee85a
starwarrs <yulikamiya@gmail.com>
2020-02-15 02:19:08 +0900

    wip

--- app/services/body/lambda.rb
+++ app/services/body/lambda.rb
@@ -21,10 +21,11 @@ module Body
         req.params[:types] = "im"
       end
 
-       question = natsuo.get do |req|
-         req.url '/api/v1/questions/default'
-         req.headers[:is_from_slack]= "true"
-       end
+      question = natsuo.get do |req|
+        req.url '/api/v1/questions/default'
+        req.headers[:is_from_slack]= "true"
+      end
+
        p "質問ありますか"
        p question
        p "質問ありますか"
@@ -37,18 +38,28 @@ module Body
       # natsuo.post '/api/chat.postMessage',body.to_json, {"workspace_id" => '???',"slack_user_id"=>"???"}
 
       hash = JSON.parse(response.body)
+      body = {
+        :token => ENV['SLACK_BOT_USER_TOKEN'],
+   #     :channel => "#{var["id"]}", 全員に対して個人DMしたくなったらこれを起動しましょう
+        :channel => "#general",
+        :text  => "あなたに質問があります"
+       
+      }
+      conn.post '/api/chat.postMessage',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}
+    
       p hash["channels"]
        for var in hash["channels"] do
          p var["id"]
-         body = {
-           :token => ENV['SLACK_BOT_USER_TOKEN'],
-      #     :channel => "#{var["id"]}", 全員に対して個人DMしたくなったらこれを起動しましょう
-           :channel => "#general",
-           :text  => "あなたに質問があります"
+      #    body = {
+      #      :token => ENV['SLACK_BOT_USER_TOKEN'],
+      # #     :channel => "#{var["id"]}", 全員に対して個人DMしたくなったらこれを起動しましょう
+      #      :channel => "#general",
+      #      :text  => "あなたに質問があります"
           
-         }
-         conn.post '/api/chat.postMessage',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}
-       end
+      #    }
+      #    conn.post '/api/chat.postMessage',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}
+       
+        end
 
     end
   end
