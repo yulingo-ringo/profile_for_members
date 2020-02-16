8d8a9f0fd72015d56c235fa69e3c0e9ef6d05dd1
starwarrs <yulikamiya@gmail.com>
2020-02-16 18:04:55 +0900

    wip

--- app/services/body/action.rb
+++ app/services/body/action.rb
@@ -120,7 +120,7 @@ module Body
                     :content => question,
                     :answer => answer
                 }
-                natsuo.put "/api/v1/questions/#{id}",body.to_json, {"workspace-id"=>@json["view"]["team_id"],"slack-user-id"=>@json["view"]["id"]}
+                natsuo.put "/api/v1/questions/#{id}",body.to_json, {"workspace-id"=>@json["view"]["team_id"],"slack-user-id"=>@json["user"]["id"]}
     
             else
             response = conn.get do |req|  
